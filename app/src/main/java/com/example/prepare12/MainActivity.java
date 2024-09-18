cdbf5c2564bde658e6568e1750960bc14dfdb39a
AliBaker123 <bkr861810@gmail.com>
2024-09-18 09:11:33 +0300

    ali-1

--- app/src/main/java/com/example/prepare12/MainActivity.java
+++ app/src/main/java/com/example/prepare12/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.prepare12;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +15,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+private Button start;
+private Button stop = findViewById(R.id.stop);
+private TextView count;
+private boolean isCom=true;
+private int count1=0;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=findViewById(R.id.start);
+        count=findViewById(R.id.count);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                while(isCom==true){
+                    count1++;
+                    count.setText(String.valueOf(count1));
+                }
+            }
+        });
+      stop.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              isCom=false;
 
+          }
+      });
     }
 }
